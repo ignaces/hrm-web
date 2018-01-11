0b551d0bc7595684623e2add5ea3311a378c3618
Andrés Santa María <aletheialego@gmail.com>
2018-01-10 22:16:28 -0300

    url

--- app/Middleware/AuthValidator.js
+++ app/Middleware/AuthValidator.js
@@ -2,9 +2,11 @@
 
 class AuthValidator {
   async handle ({ auth,view ,request,response}, next) {
-    
+    console.log(request.url())
     try {
+      
       const isLoggedIn = await auth.check()
+
       if (!isLoggedIn && request.url()!='/login' && request.url()!='/register') {// && request.url()!='/register'
       
          response.redirect('/login');
