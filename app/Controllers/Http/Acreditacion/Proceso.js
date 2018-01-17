12b19b6deb1264f1e9e687e2803a3270770de79f
jcoruiz <jcoruiz@gmail.com>
2018-01-17 16:44:23 -0300

    sdf

--- app/Controllers/Http/Acreditacion/Proceso.js
+++ app/Controllers/Http/Acreditacion/Proceso.js
@@ -3,31 +3,7 @@
 const got = use('got')
 
 class Proceso {
-<<<<<<< HEAD
     
-=======
-     index  ({ view,request, response }) {
-        var texto ="hola";
-        var salida=`aqui ${texto} texto`
-
-        console.log(salida)
-        var procesos = [{
-                    id:1,
-                    nombre:"Acreditación 2018",
-                    class:"task-info",
-                    image:"reevaluacion.png",
-                    descripcion:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie, ipsum et ullamcorper viverra, purus quam aliquam dolor, vel lacinia lectus odio porta risus. Aenean tincidunt pharetra iaculis. Pellentesque id mi magna. Ut commodo nisl in leo rutrum molestie. Nullam sed purus porta, finibus elit id, feugiat ex. Cras interdum porttitor dui vitae convallis. Mauris id magna ut turpis imperdiet auctor eget a diam."
-                },{
-                    id:2,
-                    nombre:"Reevaluación 2018",
-                    class:"task-info",
-                    image:"evaluacion.png",
-                    descripcion:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie, ipsum et ullamcorper viverra, purus quam aliquam dolor, vel lacinia lectus odio porta risus. Aenean tincidunt pharetra iaculis. Pellentesque id mi magna. Ut commodo nisl in leo rutrum molestie. Nullam sed purus porta, finibus elit id, feugiat ex. Cras interdum porttitor dui vitae convallis. Mauris id magna ut turpis imperdiet auctor eget a diam."
-                }
-        ];
-        return view.render('acreditacion/proceso/list',  {procesos:procesos});
-    }
->>>>>>> 8df3ce19bd10bd2a72068d2f90bcae087c45c17d
 
     async colaboradores ({view,request, response, auth, session}) {
         
