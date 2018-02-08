d59ef663feac9417eaa61a4c4bb625131063d552
Andres Santa Maria Norero <andres.santamaria@MacBook-Pro-de-asantamaria-mac.local>
2018-02-08 12:14:47 -0300

    proceso dinamico welcome

--- app/Controllers/Http/Acreditacion/Proceso.js
+++ app/Controllers/Http/Acreditacion/Proceso.js
@@ -8,9 +8,9 @@ class Proceso {
     async colaboradores ({view,request, response, auth, session}) {
         
         var idPersona = session.get('idPersona', 'fail')
-
+        var idProceso = request.input("proceso")
         var obj = {
-            "idProceso":"9d212163-f0e6-11e7-bf12-bc764e100f2b",
+            "idProceso":idProceso,
             "idPersona": idPersona
         };
         var result = await data.execApi(request.hostname(),'/Acreditacion/Proceso/getPersonasEvaluaciones',obj);
