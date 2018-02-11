7534288e2c869ca1d5209d8ee155725d09471663
Andres Santa Maria Norero <andres.santamaria@MacBook-Pro-de-asantamaria-mac.local>
2018-02-11 12:07:44 -0300

    cambios persona portada

--- app/Controllers/Http/Core/Portada.js
+++ app/Controllers/Http/Core/Portada.js
@@ -9,18 +9,19 @@ class Portada {
         var idPersona = session.get('idPersona', 'fall')
         var obj = {
             "idProceso":""
+        
         };
+
+        
         var result = await data.execApi(request.hostname(),'/Acreditacion/Proceso/getProcesos',obj);
+
+        
         var procesos = result.body.data.procesos;
         var user={usuario:auth.user}
-        var cargo="Evaluador FCH"
-        var genero="F"
-        var imageUser="/assets/images/icons/businessman.svg"
-        if (genero=="F"){
-            imageUser="/assets/images/icons/businesswoman.svg"
-        }
         
-        return view.render('core/welcome',  {user,cargo,genero,imageUser,procesos});
+        var persona = session.get('personaLogueada')
+
+        return view.render('core/welcome',  {user,procesos,persona});
     }   
 }
 
