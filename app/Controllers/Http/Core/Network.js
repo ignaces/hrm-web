7ef3ef0ebc8b0e8cf443343c71c62dae455a3d8f
Andrés Santa María <aletheialego@gmail.com>
2018-02-18 16:38:28 -0300

    se agrega metodo scan a network

--- app/Controllers/Http/Core/Network.js
+++ app/Controllers/Http/Core/Network.js
@@ -27,7 +27,29 @@
               return view.render('core/network/index',  {network:network});
         
         } 
+        async scan  ({ view,request, response, auth, session }) {
+          var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+          await  wifi.init({
+                iface : null // network interface, choose a random wifi interface if set to null
+            });
+            
+            var redes =[];
+            var error="";
+            const redes_ = await wifi.scan(function(err, networks) {
+              if (err) {
+                  error=err;
+              } else {
+                  redes=networks;
+                // console.log(redes)
+                
+              }
+            });
+            await wait(5000)
+              var network = {redes:redes,error:error}
+              
+              return network;
         
+        } 
         async connect  ({ view,request, response, auth, session }) {
           await  wifi.init({
             iface : null // network interface, choose a random wifi interface if set to null
