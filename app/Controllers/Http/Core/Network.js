b63a82fc6bf4d43cf7c304517a11042e5612da4c
Andrés Santa María <aletheialego@gmail.com>
2018-02-19 00:46:25 -0300

    status

--- app/Controllers/Http/Core/Network.js
+++ app/Controllers/Http/Core/Network.js
@@ -58,7 +58,7 @@
           var password = request.input("password");
           var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
           var mensaje = "OK";
-          wifi.connect({ ssid :ssid, password : password}, function(err) {
+          await wifi.connect({ ssid :ssid, password : password}, function(err) {
             if (err) {
                mensaje=err;
             }
@@ -67,6 +67,40 @@
           await wait(5000)
           return {mensaje:mensaje}
 
+        }
+
+        async getStatus({view,request, response, auth, session}){
+          var wait = ms => new Promise((r, j)=>setTimeout(r, ms))
+          await  wifi.init({
+            iface : 'mac' // network interface, choose a random wifi interface if set to null
+          });
+          var conecciones = "";
+          await wifi.getCurrentConnections(function(err, currentConnections) {
+            if (err) {
+                console.log(err);
+            }
+            
+            conecciones = currentConnections;
+            /*
+            // you may have several connections
+            [
+                {
+                    iface: '...', // network interface used for the connection, not available on macOS
+                    ssid: '...',
+                    bssid: '...',
+                    mac: '...', // equals to bssid (for retrocompatibility)
+                    channel: <number>,
+                    frequency: <number>, // in MHz
+                    signal_level: <number>, // in dB
+                    security: '...' //
+                    security_flags: '...' // encryption protocols (format currently depending of the OS)
+                    mode: '...' // network mode like Infra (format currently depending of the OS)
+                }
+            ]
+            */
+          });
+          await wait(2000)
+          return conecciones;
         }
       }
       
--- client/src/Core/Network.js
+++ client/src/Core/Network.js
@@ -1,5 +1,21 @@
-//WP
+var status = function (){
+    var obj={}
+    $.ajax({
+         type: "GET",
+         url: "/Core/Network/getStatus",
+         contentType: "application/json; charset=utf-8",
+         data: obj,
+         dataType: "json", 
+         success: function (msg) {
+            console.log(msg)
+         }
+     });   
+}
 $(document).ready(function(){
+
+    status();
+    
+
     $("#btnConectar").click(function(){
         
         var spinner= '<div class=\"sk-cube-grid\"><div class=\"sk-cube sk-cube1\"></div><div class=\"sk-cube sk-cube2\"></div><div class=\"sk-cube sk-cube3\"></div><div class=\"sk-cube sk-cube4\"></div><div class=\"sk-cube sk-cube5\"></div><div class=\"sk-cube sk-cube6\"></div><div class=\"sk-cube sk-cube7\"></div><div class=\"sk-cube sk-cube8\"></div><div class=\"sk-cube sk-cube9\"></div></div>';
