1b82b0a9a7a87a8b92ba9779856add9177aefbc5
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-02-27 12:02:48 -0300

    encuesta clima

---
+++ app/Controllers/Http/Clima/Encuesta.js
@@ -0,0 +1,17 @@
+'use strict'
+const data = use('App/Utils/Data')
+const uuidv4 = require('uuid/v4');
+const XLSX = require('xlsx');
+var Enumerable = require('linq');
+class Encuesta {
+    async index({view,request, response}) {
+        var obj = {
+            idCliente:""
+        };
+        
+        
+        
+        return view.render('encuesta/index',  {});
+    }
+}
+module.exports = Encuesta 
