a36e62c90a70e42180c3f6bf8ab45af438106918
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-03-16 12:12:56 -0300

    mess

--- app/Controllers/Http/Redes/Visualizador.js
+++ app/Controllers/Http/Redes/Visualizador.js
@@ -5,7 +5,7 @@ const data = use('App/Utils/Data')
 class Visualizador {
     async viewGraph({view,request,response,params}){
 
-        var obj={};
+        var obj={idAplicacio:"4771dc31-2621-11e8-80db-bc764e10787e"};
         var graph = await data.execApi(request.hostname(),'/Redes/Medicion/getGraph',obj);
         
         return view.render('redes/visualizacion',{graph:graph.body})
