836421e6f704e7847dceeaaea72e7937ff33522e
Julio Montaña Bengoa <julio.montana@MacBook-Pro-de-jmontana-mac.local>
2018-03-16 17:14:38 -0300

    Filtro redes

--- app/Controllers/Http/Redes/Visualizador.js
+++ app/Controllers/Http/Redes/Visualizador.js
@@ -6,6 +6,21 @@ class Visualizador {
     async viewGraph({view,request,response,params}){
 
         var obj={};
+        
+        if(request.input("id"))
+        {
+            obj.id = request.input("id");
+        }
+        
+        if(request.input("sentido"))
+        {
+            obj.sentido = request.input("sentido");
+        }
+        else
+        {
+            obj.sentido = 0;
+        }
+
         var graph = await data.execApi(request.hostname(),'/Redes/Medicion/getGraph',obj);
         
         return view.render('redes/visualizacion',{graph:graph.body})
