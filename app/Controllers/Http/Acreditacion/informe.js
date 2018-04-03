17b3aa864711d2d8cbac1ceebf59fad19fa2b0c7
= <=>
2018-04-03 14:07:39 -0300

    informe

--- app/Controllers/Http/Acreditacion/informe.js
+++ app/Controllers/Http/Acreditacion/informe.js
@@ -1,12 +1,22 @@
 'use strict'
-
+const data = use('App/Utils/Data')
 const got = use('got')
 
 class Informe {
-     index  ({ view,request, response, auth }) {
+     async index  ({ view,request, response, auth }) {
         
+        var obj = {
+            "idProceso":"s",
+            "procesoPersona": "2c58a181-2311-11e8-80db-bc764e10787e"
+        };
+
+        var resultSintesis = await data.execApi(request.hostname(),'/Acreditacion/Informe/getResultadoSistesis',obj);
+        var resultadoSintesis = resultSintesis.body.data;
+
+        var resultTCO = await data.execApi(request.hostname(),'/Acreditacion/Informe/getResultadoTCO',obj);
+        var resultadoTCO = resultTCO.body.data;
        
-        return view.render('acreditacion/informe/informesd');
+        return view.render('acreditacion/informe/informesd', {sintesis:resultadoSintesis, resultadoTCO:resultadoTCO});
     }   
 }
 
