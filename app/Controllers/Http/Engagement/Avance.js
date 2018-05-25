b36182eedfcbb27fc045693e9b1679acf3a50753
Andres Santa Maria Norero <andres.santamaria@asantamaria-mac.local>
2018-05-25 01:17:10 -0400

    stack eng

--- app/Controllers/Http/Engagement/Avance.js
+++ app/Controllers/Http/Engagement/Avance.js
@@ -1,7 +1,7 @@
 'use strict'
 
     const data = use('App/Utils/Data')
-    
+    var Enumerable = require('linq');
     class Avance {
          async index  ({ view,request, response, auth, session }) {
 
@@ -26,12 +26,55 @@
             var id = request.input("idEncuestaAplicacion");
             
             var resumen = await data.execApi(request.hostname(),'/Encuesta/Avance/getAvanceResumen',{idEncuestaAplicacion:id});  
+            
             var detalleClasificaciones = await data.execApi(request.hostname(),'/Engagement/Empresa/getAvanceClasificaciones',{idEncuestaAplicacion:id});  
+            
+            
+            
+            var clasificaciones =Enumerable.from(detalleClasificaciones.body).distinct("$.id").select(function(clasificacion){
+                                        return{
+                                            id:clasificacion.id,
+                                            nombre:clasificacion.nombre,
+                                            niveles:Enumerable.from(clasificacion.niveles).distinct("$.id").select(function(nivel){
+                                                        return nivel.nombre
+                                                        
+                                                    }).toArray(),
+                                            estados:Enumerable.from(clasificacion.niveles[0].estados).select(function(estado){
+                                                
+                                                        return{
+                                                            texto:estado.texto,
+                                                            color:estado.color,
+                                                            class:estado.class,
+                                                            codigo:estado.codigo,
+                                                            data:Enumerable.from(clasificacion.niveles).select(function(data){
+                                                                var cantidad = 0;
+                                                                for(var i in data.estados){
+
+                                                                    if(data.estados[i].codigo==estado.codigo){
+                                                                        
+                                                                        cantidad= data.estados[i].cantidad
+                                                                    }
+                                                                }
+
+                                                                return cantidad;
+                                                                
+                                                                
+                                                                
+                                                            }).toArray(),
+                                                        }
+                                                    }).toArray(),
+                                            
+
+                                        }
+                                 }).toArray();
+            
+            
+
 
             return view.render('encuesta/avance/index',  {
                 resumen:resumen.body,
                 idEncuestaAplicacion:id,
-                clasificaciones:detalleClasificaciones
+                clasificaciones:clasificaciones
             });
           
         } 
