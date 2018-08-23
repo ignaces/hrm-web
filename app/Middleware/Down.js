09394fef3223deec3b901f10c11497c3b460831a
enovum <mauricio.rodriguez@fch.cl>
2018-08-23 15:53:16 -0300

    regla

---
+++ app/Middleware/Down.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class Down {
+  async handle ({ auth,view ,request,response,session}, next) {
+    
+    try {
+      
+      const isLoggedIn = await auth.check()
+      request.session=session;
+      if (!isLoggedIn && request.url()!='/login' && request.url()!='/register') {// && request.url()!='/register'
+      
+         response.redirect('/login');
+ 
+      }
+    } catch (error) {
+      response.redirect('/login');
+    }
+    //var uu = await request.session.get('roles');
+
+    await next()
+  }
+}
+
+module.exports = Down
