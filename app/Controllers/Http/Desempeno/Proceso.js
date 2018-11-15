c62e20d7cb07b81e669b3157f617256ede24179f
Jorge Luis Leiva Silva <jlleivas@vectoritcgroup.com>
2018-11-15 16:33:06 -0300

    (jlleivas)

--- app/Controllers/Http/Desempeno/Proceso.js
+++ app/Controllers/Http/Desempeno/Proceso.js
@@ -259,9 +259,9 @@ class Proceso {
         
         /* BEGIN TEST CALAIBRACION */
         var objCal={
-            "idEtapa":"" ,
-	        "idPersonaActor":"",
-            "codigoActor":"SUPE",
+            "idEtapa":idEtapa ,
+	        "idPersonaActor":idPersona,
+            "codigoActor":"CALI",
             "idAccionPersona":""
         }
         var resultCal;
@@ -288,14 +288,9 @@ class Proceso {
             "CA":0,
             "CB":0,
             "CC":1
-        }
-        if(idEtapa === '5ccf2775-c74a-11e8-8771-bc764e100f2b'){
-            objCal.idEtapa = '1f05c0a0-c70e-11e8-8771-bc764e100f2b';
-            objCal.idPersonaActor = idPersona;
-            resultCal=await api.execApi(request.hostname(),'/Desempeno/Proceso/getListaEvaluados',objCal);
-            listaCal =resultCal.body.data;
-            console.log('TAMAÑO:'+listaCal.length+' idEtapa:'+objCal.idEtapa); 
-        }
+        }          
+        resultCal=await api.execApi(request.hostname(),'/Desempeno/Proceso/getListaCalibracion',objCal);
+        listaCal =resultCal.body.data;
         /* END TEST CALAIBRACION */  
         //
         //////console.log(objSupe)
@@ -343,7 +338,11 @@ class Proceso {
         
 
         console.log(listaParams);
-        return view.render('desempeno/etapa',{etag, datosProceso,PersonaEde,datosMenu,etapa,listaEval,listaSupe,listaAsc,listaFunc,listaCal, idEtapa: idEtapa, params: listaParams, matrizEval:matrizEval,matrizCalib:matrizCalib});
+        
+        return view.render('desempeno/etapa',
+        {etag, datosProceso,PersonaEde,datosMenu,
+            etapa,listaEval,listaSupe,listaAsc,listaFunc,
+            listaCal, idEtapa: idEtapa, params: listaParams, matrizEval:matrizEval,matrizCalib:matrizCalib});
     }
 
     async evalBrasil ({view,request, response}) {
