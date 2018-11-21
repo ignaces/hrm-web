c79bfff82d2328f337c1449757869fb318fbd921
Jorge Luis Leiva Silva <jlleivas@vectoritcgroup.com>
2018-11-21 15:57:45 -0300

    refactoring calibracion

--- app/Controllers/Http/Desempeno/CalibracionController.js
+++ app/Controllers/Http/Desempeno/CalibracionController.js
@@ -6,7 +6,6 @@ const Antl = use('Antl');
 class CalibracionController {
     async updateCalibracion({request, response}) {
 try {
-        console.log('******entro hrm updateCalibracion');
         var idEdeEtapaTareaActor= request.input("idEdeEtapaTareaActor");
         var calibracion= request.input("calibracion");
         
@@ -16,7 +15,6 @@ try {
         };
 
         var result = await api.execApi(request.hostname(),'/Desempeno/Proceso/updateCalibracion',obj);  
-        console.log('******salio hrm updateCalibracion');
     } catch (e) {
         console.log(e);
         
