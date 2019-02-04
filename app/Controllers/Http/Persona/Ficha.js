08e683c57c14bb6e25bfe830073ae9d55d53f128
juandpm <juand.pm1992@gmail.com>
2019-02-04 16:10:21 -0300

    editar cv ficha

--- app/Controllers/Http/Persona/Ficha.js
+++ app/Controllers/Http/Persona/Ficha.js
@@ -3,19 +3,73 @@
 const data = use('App/Utils/Data')
 
 class Ficha {
-   async index ({view,request, response, auth,session}) {
+   
+    async index ({view,request, response, auth,session}) {
 
+        var persona =  session.get('personaLogueada')
+                
+        var resultPersona =  await data.execApi(request.hostname(),'/Persona/Ficha/getPersonaDetalle',{idPersona:persona.id});
+        var pers = resultPersona.body;
 
-            var persona =  session.get('personaLogueada')
-            
-            var obj = {
-                "idPersona":persona.id
-            };
-            
-            var resultPersona =  await data.execApi(request.hostname(),'/Persona/Persona/getPersona',obj);
-           
-        return view.render('persona/ficha',  {persona:persona});
+        var objExp = {
+            "idPersona":persona.id
+        };
+
+        var resultCv =  await data.execApi(request.hostname(),'/Persona/Ficha/getPersonaCurriculum',objExp);
+        var personaCv = resultCv.body;
+
+        return view.render('persona/ficha',  {persona:pers[0],personaCv:personaCv});
     }
+
+    async updateCv ({ view,request, response, auth, session }) {
+        var cv = request.input("cv");
+        var persona =  session.get('personaLogueada')
+
+        var obj = {
+            idPersona:persona.id,
+            objCv:cv
+        }
+
+        var result = await data.execApiPost(request.hostname(),'/Persona/Ficha/updateCv',obj);
+
+        var cv = result.body.data;
+
+        return cv;
+    } 
+
+    async addCv ({ view,request, response, auth, session }) {
+        var cv = request.input("cv");
+        var persona =  session.get('personaLogueada')
+
+        var obj = {
+            idPersona:persona.id,
+            objCv:cv
+        }
+
+        var result = await data.execApiPost(request.hostname(),'/Persona/Ficha/addCv',obj);
+
+        var cv = result.body.data;
+
+        return cv;
+    } 
+
+    async deleteCv ({ view,request, response, auth, session }) {
+        var cv = request.input("cv");
+        var persona =  session.get('personaLogueada')
+
+        var obj = {
+            idPersona:persona.id,
+            objCv:cv
+        }
+
+        var result = await data.execApiPost(request.hostname(),'/Persona/Ficha/deleteCv',obj);
+
+        var cv = result.body.data;
+
+        return cv;
+    } 
+
+    /****/
      historia ({view,request,response}){
          const Env = use('Env')
          
