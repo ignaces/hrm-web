4f66c86d69b2a9f504e7a869f6edea29aecc9435
juandpm <juand.pm1992@gmail.com>
2019-02-26 14:57:46 -0300

    bitacora

--- app/Controllers/Http/Core/Herramientas.js
+++ app/Controllers/Http/Core/Herramientas.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const api = use('App/Utils/Data')
+const got = use('got')
+const FormData = require('form-data');
+const fs = require('fs');
   
 class Herramientas {
 
@@ -16,6 +19,140 @@ class Herramientas {
     return view.render('core/herramientas/documentos', {datosDocs});
 
         
+    }
+
+    async bitacora  ({ view,request,session, response }) {
+        var idPersona = session.get('idPersona', 'fall')
+
+        var result = await api.execApi(request.hostname(), '/Core/Bitacora/getPersona', {idPersona:idPersona});
+        var persona = result.body.data;
+
+        var resultq = await api.execApi(request.hostname(), '/Core/Bitacora/getListEquipo', {idPersona:idPersona});
+        var equipo = resultq.body.data;
+
+        return view.render('core/herramientas/bitacora',{Persona:persona,equipo:equipo});
+    }
+
+    async bitacoraPersona  ({ view,request,session, response }) {
+        var idOpinado = request.input("idPersona")
+        var idOpinante = session.get('idPersona', 'fall')
+
+        var obj = {
+            idOpinado:idOpinado,
+            idOpinante:idOpinante
+        }
+
+        var result = await api.execApi(request.hostname(), '/Core/Bitacora/getBitacoraPersona', obj);
+        var bitacora = result.body.data;
+
+        return view.render('core/herramientas/bitacoraPersona',{bitacora:bitacora,idOpinado:idOpinado});
+    }
+
+    async addItem({ view,request,session, response }) {
+        var idOpinante = session.get('idPersona', 'fall')
+        var idOpinado = request.input("idOpinado")
+        var titulo = request.input("titulo")
+        var descripcion = request.input("descripcion")
+        var archivo = request.input("archivo")
+        var link = request.input("link")
+
+        var obj = {
+            idOpinado:idOpinado,
+            idOpinante:idOpinante,
+            titulo:titulo,
+            descripcion:descripcion,
+            archivo:archivo,
+            link:link
+        }
+
+        var objb = {
+            idOpinado:idOpinado,
+            idOpinante:idOpinante
+        }
+
+        var result = await api.execApi(request.hostname(), '/Core/Bitacora/addItem', obj);
+        var resultb = await api.execApi(request.hostname(), '/Core/Bitacora/getBitacoraPersona', objb);
+        var bitacora = resultb.body.data;
+
+        return bitacora;
+    }
+
+    async deleteItem({ view,request,session, response }) {
+        var id = request.input("id")
+        var idOpinante = session.get('idPersona', 'fall')
+        var idOpinado = request.input("idOpinado")
+
+        var obj = {
+            idOpinado:idOpinado,
+            idOpinante:idOpinante
+        }
+
+        var result = await api.execApi(request.hostname(), '/Core/Bitacora/deleteItem', {id:id});
+        var resultb = await api.execApi(request.hostname(), '/Core/Bitacora/getBitacoraPersona', obj);
+        var bitacora = resultb.body.data;
+
+        return bitacora;
+    }
+
+    async editItem({ view,request,session, response }) {
+        var id = request.input("id")
+        var idOpinante = session.get('idPersona', 'fall')
+        var idOpinado = request.input("idOpinado")
+        var titulo = request.input("titulo")
+        var descripcion = request.input("descripcion")
+        var archivo = request.input("archivo")
+        var link = request.input("link")
+
+        var obj = {
+            id:id,
+            idOpinado:idOpinado,
+            idOpinante:idOpinante,
+            titulo:titulo,
+            descripcion:descripcion,
+            archivo:archivo,
+            link:link
+        }
+
+        var objb = {
+            idOpinado:idOpinado,
+            idOpinante:idOpinante
+        }
+
+        var result = await api.execApi(request.hostname(), '/Core/Bitacora/editItem', obj);
+        var resultb = await api.execApi(request.hostname(), '/Core/Bitacora/getBitacoraPersona', objb);
+        var bitacora = resultb.body.data;
+
+        return bitacora;
+    }
+
+    async loadFile({ view,request,session, response }) {
+        var file = request.file("file");
+        var cliente=request.hostname();
+        var form = new FormData();
+        var url = '';
+
+        try{
+            form.append('file', fs.createReadStream(file._tmpPath));
+            form.append('cliente', cliente);
+            form.append('subtype', file._clientName.split('.')[1]);
+
+            console.log(file._clientName.split('.')[1])
+
+            var result = await got.post('http://127.0.0.1:3333/Files/File/uploadFile', {
+                body: form
+            });
+            url = result.body;
+
+            fs.unlink(file._tmpPath)
+            
+        }catch(ex){
+            console.log(ex.message)
+        }
+
+        return {
+            fileName:file._clientName,
+            fileLink:url
+        }
     }
 }
 
