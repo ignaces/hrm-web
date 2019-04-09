516a6538f5cc135309f1178a6e393f099dec730a
juandpm <juand.pm1992@gmail.com>
2019-04-09 12:13:22 -0400

    getMetasDetalles

--- app/Controllers/Http/Meta/Meta.js
+++ app/Controllers/Http/Meta/Meta.js
@@ -10,11 +10,24 @@ class Meta {
         var resTAB = await data.execApi(request.hostname(),'/Incentivos/Incentivos/getMetasAttr_CG',{param:'TAB'});
         var resTIP = await data.execApi(request.hostname(),'/Incentivos/Incentivos/getMetasAttr_CG',{param:'TIP'});
 
+        var metaDetalle = await data.execApi(request.hostname(),'/Incentivos/Incentivos/getMetasDetalles',{idPeriodo:'37a0d776-5a64-11e9-80db-bc764e10787e'});
+
         return view.render('metas/metas',{
             resPER:resPER.body,
             resTAB:resTAB.body,
-            resTIP:resTIP.body
+            resTIP:resTIP.body,
+            metaDetalle:metaDetalle.body.data
         });
+    }
+
+    async consulaMetasDetalles ({ view,request, response, auth }) {
+        var idPeriodo = request.input('idPeriodo');
+
+        var metaDetalle = await data.execApi(request.hostname(),'/Incentivos/Incentivos/getMetasDetalles',{idPeriodo:idPeriodo});
+
+        return {
+            metaDetalle:metaDetalle.body.data
+        };
     }
 
     async carga ({ view,request, response, auth }) {
